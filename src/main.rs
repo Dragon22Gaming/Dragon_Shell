856359aa5f2b3d19674512a25b1233b0807a0096
DragonDev22 <dragon22gaming.yt@gmail.com>
2024-09-25 14:10:21 +0930

    Set up functions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
-use std::io;
+use std::io::{self, stdout, Write};
 
 fn main() {
     let entrance_string: &str = "Welcome to DragonShell Version:";
     let version: &str = "0.01";
     //print both of the strings
-    println!("{}", entrance_string.to_owned().to_string() + " " + version);
-    get_input();
+    println!("{}", entrance_string.to_string() + " " + version);
+    loop {
+        print!("$ ");
+        stdout().flush().unwrap();
+        get_input();
+    }
 }
 
 fn get_input() {
-    let mut input = String::new();
-    println!("*input noises*");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("{}", input);
+    let mut input: String = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    handle_commands(&input);
+}
+
+fn handle_commands(input: &str) {
+    //let mut command_string: String = String::new();
+    println!("You Entered:");
+    print!("{}", input);
+    
+    /*if input.starts_with("cd ") {
+    }*/
 }
